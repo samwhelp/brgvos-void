5fe9facea71346d55f8d7d2ddfeb9620e8c1e7dc
Florin Tanasa <florin.tanasa@gmail.com>
2025-07-31 14:23:26 +0300

    Implements notification position customization
    Adds functionality to change the position of notification banners on the screen.
    
    This introduces a settings schema and preferences window, allowing users to select from nine different positions (right, middle, left; top, center, bottom). The extension persists the chosen position and applies it to the notification banner.

--- set-notification-position@brgvos/extension.js
+++ set-notification-position@brgvos/extension.js
@@ -1,13 +1,81 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 import Clutter from 'gi://Clutter';
+import Gio from 'gi://Gio';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-export default class NotificationPosition {
-    constructor() {
+export default class NotificationPosition extends Extension {
+    _settings;
+    _preferences;
+    _bannerPositionChangedId = 0;
+    constructor(metadata) {
+        super(metadata);
         this._originalBannerAlignment = Main.messageTray.bannerAlignment;
         this._originalYAlign = Main.messageTray.actor.get_y_align();
     }
+        enable() {
+        // Get Preferences
+        this._preferences = this.getSettings();
+        // Connect to cahnge banner position cahnges
+        this._bannerPositionChangedId = this._preferences.connect("changed::change-banner-position", this._onChangeBannerPosition.bind(this));
+        // Initial notification banner position
+        this._onChangeBannerPosition();
+    
+    }
+    disable() {
+        this._original();
+    }
+    _onChangeBannerPosition() {
+        // Get current position
+        const bannerPositon = this._preferences?.get_int('change-banner-position');
+        // check and chose the method
+        if (bannerPositon == 0) {
+            this.rightBottom();
+        }
+        else if (bannerPositon == 1) {
+            this.rightCenter();
+        }
+        else if (bannerPositon == 2) {
+            this.rightUpper();
+        }
+        else if (bannerPositon == 3) {
+            this.middleBottom();
+        }
+        else if (bannerPositon == 4) {
+            this.middleCenter();
+        }
+        else if (bannerPositon == 5) {
+            this.middleUpper();
+        }
+        else if (bannerPositon == 6) {
+            this.leftBottom();
+        }
+        else if (bannerPositon == 7) {
+            this.leftCenter();
+        }        
+        else if (bannerPositon == 8) {
+            this.leftUpper();
+        }
+    }
     leftUpper() {
         Main.messageTray.bannerAlignment = Clutter.ActorAlign.START;
+        Main.messageTray.actor.set_y_align(Clutter.ActorAlign.START);
     }
     leftCenter() {
         Main.messageTray.bannerAlignment = Clutter.ActorAlign.START;
@@ -19,6 +87,7 @@ export default class NotificationPosition {
     }
     rightUpper() {
         Main.messageTray.bannerAlignment = Clutter.ActorAlign.END;
+        Main.messageTray.actor.set_y_align(Clutter.ActorAlign.START);
     }
     rightCenter() {
         Main.messageTray.bannerAlignment = Clutter.ActorAlign.END;
@@ -30,6 +99,7 @@ export default class NotificationPosition {
     }
     middleUpper() {
         Main.messageTray.bannerAlignment = Clutter.ActorAlign.CENTER;
+        Main.messageTray.actor.set_y_align(Clutter.ActorAlign.START);
     }
     middleCenter() {
         Main.messageTray.bannerAlignment = Clutter.ActorAlign.CENTER;
@@ -42,11 +112,5 @@ export default class NotificationPosition {
     _original() {
         Main.messageTray.bannerAlignment = this._originalBannerAlignment;
         Main.messageTray.actor.set_y_align(this._originalYAlign);
-    }
-    enable() {
-        this.rightBottom();
-    }
-    disable() {
-        this._original();
     }
 }
--- set-notification-position@brgvos/prefs.js
+++ set-notification-position@brgvos/prefs.js
@@ -1,21 +1,59 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 import Adw from 'gi://Adw';
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import Gtk from 'gi://Gtk';
 import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
 export default class SetNotificationPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
         const preferences = this.getSettings();
+        // Create a preferences page, with a single group
         const page = new Adw.PreferencesPage({
             title: _('General'),
             iconName: 'dialog-information-symbolic',
         });
+        window.add(page);
         const GeneralGroup = new Adw.PreferencesGroup({
-            title: _('General'),
-            description: _('Configure General Options'),
+            title: _('Notification Position'),
+            description: _('Setting notification banner position on screen'),
         });
         page.add(GeneralGroup);
-        const row = new Adw.ComboRow();
+        const items = Gtk.StringList.new([
+            _('Right Bottom'),
+            _('Right Center'),
+            _('Right Upper'),
+            _('Middle Bottom'),
+            _('Middle Center'),
+            _('Middle Upper'),
+            _('Left Bottom'),
+            _('Left Center'),
+            _('Left Upper'),
+        ]);
+        // Create a new preferences row
+        const row = new Adw.ComboRow({
+            model: items,
+            selected: preferences.get_int('change-banner-position'),
+        });
         GeneralGroup.add(row);
+        row.connect('notify::selected', () => {
+            preferences.set_int('change-banner-position', row.selected);
+        });
     }
 }
