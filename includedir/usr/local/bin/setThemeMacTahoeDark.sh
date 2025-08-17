4af70c4af5c89c3a620fbdd1c0c5ea60adbff445
Florin Tanasa <florin.tanasa@gmail.com>
2025-08-17 22:19:45 +0300

    Adds MacTahoe dark theme setup script
    Adds a script to configure the system theme to MacTahoe dark, including enabling/disabling extensions, setting color schemes, cursor themes, GTK themes, and icon themes.

---
+++ includedir/usr/local/bin/setThemeMacTahoeDark.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# disable extensions used in Fluent
+gnome-extensions disable 'mediacontrols@cliffniff.github.com'
+gnome-extensions disable 'arcmenu@arcmenu.com'
+gnome-extensions disable 'dash-to-panel@jderose9.github.com'
+
+# enable extensions used in MacTahoe
+gnome-extensions enable 'dash-to-dock@micxgx.gmail.com'
+gnome-extensions enable 'logomenu@aryan_k'
+gnome-extensions enable 'space-bar@luchrioh'
+
+# set interface to Light theme
+dconf write /org/gnome/desktop/interface/accent-color "'red'"
+dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+dconf write /org/gnome/desktop/interface/cursor-theme "'MacTahoe-dark-cursors'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'MacTahoe-Dark-red'"
+dconf write /org/gnome/desktop/interface/icon-theme "'MacTahoe-red-dark'"
+
+# set user theme for MacTahoe
+dconf write /org/gnome/shell/extensions/user-theme/name "'MacTahoe-Dark-red'"
+
+# set theme for accent-gtk-theme
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/blue-theme-dark "'MacTahoe-Dark-blue'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/blue-theme-light "'MacTahoe-Light-blue'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/green-theme-dark "'MacTahoe-Dark-green'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/green-theme-light "'MacTahoe-Light-grey'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/orange-theme-dark "'MacTahoe-Dark-orange'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/orange-theme-light "'MacTahoe-Light-orange'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/pink-theme-dark "'MacTahoe-Dark-pink'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/pink-theme-light "'MacTahoe-Light-pink'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/purple-theme-dark "'MacTahoe-Dark-purple'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/purple-theme-light "'MacTahoe-Light-purple'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/red-theme-dark "'MacTahoe-Dark-red'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/red-theme-light "'MacTahoe-Light-red'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/slate-theme-dark "'MacTahoe-Dark-grey'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/slate-theme-light "'MacTahoe-Light-grey'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/teal-theme-dark "'MacTahoe-Dark-blue'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/teal-theme-light "'MacTahoe-Light-blue'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/yellow-theme-dark "'MacTahoe-Dark-yellow'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/yellow-theme-light "'MacTahoe-Light-yellow'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/set-link-gtk4 "true"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/set-theme-path "'/usr/share/themes'"
+
+# set theme MacTahoe for accent-user-theme
+dconf write /org/gnome/shell/extensions/accent-user-theme/blue-theme-dark "'MacTahoe-Dark-blue'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/blue-theme-light "'MacTahoe-Light-blue'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/green-theme-dark "'MacTahoe-Dark-green'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/green-theme-light "'MacTahoe-Light-grey'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/orange-theme-dark "'MacTahoe-Dark-orange'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/orange-theme-light "'MacTahoe-Light-orange'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/pink-theme-dark "'MacTahoe-Dark-pink'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/pink-theme-light "'MacTahoe-Light-pink'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/purple-theme-dark "'MacTahoe-Dark-purple'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/purple-theme-light "'MacTahoe-Light-purple'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/red-theme-dark "'MacTahoe-Dark-red'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/red-theme-light "'MacTahoe-Light-red'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/slate-theme-dark "'MacTahoe-Dark-grey'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/slate-theme-light "'MacTahoe-Light-grey'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/teal-theme-dark "'MacTahoe-Dark-blue'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/teal-theme-light "'MacTahoe-Light-blue'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/yellow-theme-dark "'MacTahoe-Dark-yellow'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/yellow-theme-light "'MacTahoe-Light-yellow'"
+
+# set MacTahoe icons for accent-icons-theme
+dconf write /org/gnome/shell/extensions/accent-icons-theme/blue-theme-dark "'MacTahoe-blue-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/blue-theme-light "'MacTahoe-blue-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/green-theme-dark "'MacTahoe-green-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/green-theme-light "'MacTahoe-green-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/orange-theme-dark "'MacTahoe-orange-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/orange-theme-light "'MacTahoe-orange-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/pink-theme-dark "'MacTahoe-purple-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/pink-theme-light "'MacTahoe-purple-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/purple-theme-dark "'MacTahoe-purple-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/purple-theme-light "'MacTahoe-purple-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/red-theme-dark "'MacTahoe-red-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/red-theme-light "'MacTahoe-red-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/slate-theme-dark "'MacTahoe-grey-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/slate-theme-light "'MacTahoe-grey-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/teal-theme-dark "'MacTahoe-nord-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/teal-theme-light "'MacTahoe-nord-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/yellow-theme-dark "'MacTahoe-yellow-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/yellow-theme-light "'MacTahoe-yellow-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/change-app-colors "false"
+
+# set MacTahoe light-dark-cursor-theme
+dconf write /org/gnome/shell/extensions/light-dark-cursor-theme/default "'MacTahoe-cursors'"
+dconf write /org/gnome/shell/extensions/light-dark-cursor-theme/prefer-dark "'MacTahoe-dark-cursors'"
+
+# set position on panel for openweatherrefined
+dconf write /org/gnome/shell/extensions/openweatherrefined/position-in-panel "'center'"
+
+# in Wayland we can't restart gnome-shell so next message work only X
+#killall -HUP gnome-shell
+
+# so in Wayland logout is solution with prompt maybe is necessary to save something
+gnome-session-quit
