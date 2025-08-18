7ece4568f475a63f8df979b08ad99639acced75d
Florin Tanasa <florin.tanasa@gmail.com>
2025-08-18 13:57:34 +0300

    Adds script to set MacTahoe Solid Light theme
    Adds a script that configures the system to use the MacTahoe Solid Light theme.
    
    This script disables extensions conflicting with the theme, enables
    theme-specific extensions, and sets various dconf settings to
    apply the theme and related customizations for GTK, icon, cursor themes,
    and shell extensions related to accent colors and theme settings. It also
    configures the position of the OpenWeatherRefined extension, and sets the
    appearance of the space-bar extension.
    
    It also includes a commented-out section to restart gnome-shell (for X11)
    and a gnome-session-quit command (for Wayland), to apply changes requiring
    a restart.

---
+++ includedir/usr/local/bin/setThemeMacTahoeSolidLight.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# disable extensions used in Fluent
+gnome-extensions disable 'mediacontrols@cliffniff.github.com'
+gnome-extensions disable 'arcmenu@arcmenu.com'
+gnome-extensions disable 'dash-to-panel@jderose9.github.com'
+
+# enable extensions used in MacTahoe
+gnome-extensions enable 'dash-to-dock@micxgx.gmail.com'
+gnome-extensions enable 'logomenu@aryan_k'
+gnome-extensions enable 'space-bar@luchrioh'
+
+# set interface to Light theme
+dconf write /org/gnome/desktop/interface/accent-color "'blue'"
+dconf write /org/gnome/desktop/interface/color-scheme "'default'"
+dconf write /org/gnome/desktop/interface/cursor-theme "'MacTahoe-cursors'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'MacTahoe-Light-solid-blue'"
+dconf write /org/gnome/desktop/interface/icon-theme "'MacTahoe-blue-light'"
+
+# set user theme for MacTahoe
+dconf write /org/gnome/shell/extensions/user-theme/name "'MacTahoe-Ligt-solid-blue'"
+
+# set theme for accent-gtk-theme
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/blue-theme-dark "'MacTahoe-Dark-solid-blue'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/blue-theme-light "'MacTahoe-Light-solid-blue'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/green-theme-dark "'MacTahoe-Dark-solid-green'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/green-theme-light "'MacTahoe-Light-solid-grey'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/orange-theme-dark "'MacTahoe-Dark-solid-orange'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/orange-theme-light "'MacTahoe-Light-solid-orange'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/pink-theme-dark "'MacTahoe-Dark-solid-pink'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/pink-theme-light "'MacTahoe-Light-solid-pink'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/purple-theme-dark "'MacTahoe-Dark-solid-purple'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/purple-theme-light "'MacTahoe-Light-solid-purple'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/red-theme-dark "'MacTahoe-Dark-solid-red'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/red-theme-light "'MacTahoe-Light-solid-red'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/slate-theme-dark "'MacTahoe-Dark-solid-grey'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/slate-theme-light "'MacTahoe-Light-solid-grey'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/teal-theme-dark "'MacTahoe-Dark-solid-nord'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/teal-theme-light "'MacTahoe-Light-solid-nord'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/yellow-theme-dark "'MacTahoe-Dark-solid-yellow'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/yellow-theme-light "'MacTahoe-Light-solid-yellow'"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/set-link-gtk4 "true"
+dconf write /org/gnome/shell/extensions/accent-gtk-theme/set-theme-path "'/usr/share/themes'"
+
+# set theme MacTahoe for accent-user-theme
+dconf write /org/gnome/shell/extensions/accent-user-theme/blue-theme-dark "'MacTahoe-Dark-solid-blue'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/blue-theme-light "'MacTahoe-Light-solid-blue'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/green-theme-dark "'MacTahoe-Dark-solid-green'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/green-theme-light "'MacTahoe-Light-solid-grey'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/orange-theme-dark "'MacTahoe-Dark-solid-orange'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/orange-theme-light "'MacTahoe-Light-solid-orange'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/pink-theme-dark "'MacTahoe-Dark-solid-pink'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/pink-theme-light "'MacTahoe-Light-solid-pink'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/purple-theme-dark "'MacTahoe-Dark-solid-purple'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/purple-theme-light "'MacTahoe-Light-solid-purple'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/red-theme-dark "'MacTahoe-Dark-solid-red'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/red-theme-light "'MacTahoe-Light-solid-red'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/slate-theme-dark "'MacTahoe-Dark-solid-grey'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/slate-theme-light "'MacTahoe-Light-solid-grey'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/teal-theme-dark "'MacTahoe-Dark-solid-nord'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/teal-theme-light "'MacTahoe-Light-nord'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/yellow-theme-dark "'MacTahoe-Dark-solid-yellow'"
+dconf write /org/gnome/shell/extensions/accent-user-theme/yellow-theme-light "'MacTahoe-Light-solid-yellow'"
+
+# set MacTahoe icons for accent-icons-theme
+dconf write /org/gnome/shell/extensions/accent-icons-theme/blue-theme-dark "'MacTahoe-blue-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/blue-theme-light "'MacTahoe-blue-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/green-theme-dark "'MacTahoe-green-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/green-theme-light "'MacTahoe-green-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/orange-theme-dark "'MacTahoe-orange-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/orange-theme-light "'MacTahoe-orange-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/pink-theme-dark "'MacTahoe-purple-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/pink-theme-light "'MacTahoe-purple-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/purple-theme-dark "'MacTahoe-purple-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/purple-theme-light "'MacTahoe-purple-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/red-theme-dark "'MacTahoe-red-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/red-theme-light "'MacTahoe-red-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/slate-theme-dark "'MacTahoe-grey-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/slate-theme-light "'MacTahoe-grey-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/teal-theme-dark "'MacTahoe-nord-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/teal-theme-light "'MacTahoe-nord-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/yellow-theme-dark "'MacTahoe-yellow-dark'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/yellow-theme-light "'MacTahoe-yellow-light'"
+dconf write /org/gnome/shell/extensions/accent-icons-theme/change-app-colors "false"
+
+# set MacTahoe light-dark-cursor-theme
+dconf write /org/gnome/shell/extensions/light-dark-cursor-theme/default "'MacTahoe-cursors'"
+dconf write /org/gnome/shell/extensions/light-dark-cursor-theme/prefer-dark "'MacTahoe-dark-cursors'"
+
+# set position on panel for openweatherrefined
+dconf write /org/gnome/shell/extensions/openweatherrefined/position-in-panel "'center'"
+
+# set space-bar appearence
+dconf write /org/gnome/shell/extensions/space-bar/appearance/active-workspace-text-color "'rgb(0,0,0)'"
+dconf write /org/gnome/shell/extensions/space-bar/appearance/inactive-workspace-text-color "'rgb(94,92,100)'"
+
+# in Wayland we can't restart gnome-shell so next message work only X
+#killall -HUP gnome-shell
+
+# so in Wayland logout is solution with prompt maybe is necessary to save something
+gnome-session-quit
