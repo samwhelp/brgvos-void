c8db30834dd970377fc1e2acd53a8dfc6e58343d
Florin Tanasa <florin.tanasa@gmail.com>
2025-08-20 09:42:08 +0300

    Configures flatpak permissions on first login.
    Adds a script to configure flatpak filesystem permissions on the user's first login.  This ensures that necessary directories are accessible to flatpak applications. Creates a file to prevent the script from running again on subsequent logins, unless explicitly reset. Provides instructions within the file for resetting the flatpak configuration.

---
+++ includedir/etc/profile.d/runonce.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This file is normally executed at first log in
+
+if [ -e $HOME/.runoance ]
+then
+    echo "Do nothing"
+else
+    flatpak override --user --filesystem=xdg-config/gtk-3.0
+    flatpak override --user --filesystem=xdg-config/gtk-4.0
+    touch $HOME/.runoance
+    cat << 'EOF' > $HOME/.runoance
+    ############################################################
+    ###                                                      ###
+    ### This file was created by /etc/profile.d/runoance.sh  ###
+    ### Look on the file what commands is run. If you        ###
+    ### delete this file at next logon commands is run       ###
+    ### again at next log in                                 ###
+    ### If you need do stop this delte or move file.         ###
+    ###                                                      ###
+    ### If you wish to reset flatpak commands run next       ###
+    ### flatpak override --user --reset                      ###
+    ###                                                      ###
+    ############################################################OD
+
+EOF
+fi
